17213aad1437914078fb88ce280cae4dfd726376
schuettec <cschue88@gmail.com>
2023-01-30 17:08:20 +0100

    Added notification.

--- start-backup-lvm.sh
+++ start-backup-lvm.sh
@@ -24,4 +24,44 @@ then
         exit 1
 fi
 
+##--------------------------------------------------------------------------------------------------------------
+## Check all commands the script needs:
+
+
+## Check LVM support installed?
+if ! command -v lvcreate &> /dev/null
+then
+    echo "LVM commands could not be found. Is LVM support installed?"
+    echo "To install LVM support run:"
+    echo "  sudo apt-get install lvm2"
+    exit 1
+fi
+
+## Check FSArchiver installed?
+if ! command -v fsarchiver &> /dev/null
+then
+    echo "FSarchiver could not be found. FSarchiver needs to be installed."
+    echo "To install run:"
+    echo "  sudo apt-get install fsarchiver"
+    exit 1
+fi
+
+##--------------------------------------------------------------------------------------------------------------
+## Maybe we can work with systemd inhibitors to avoid someone shuts down the system during backup
+## systemd-inhibit --why="Doing weekly backup" bash ./backup-lvm.sh  2>&1 | tee -a "${BACKDIR}/${BACKNAM}.log"
+
+##--------------------------------------------------------------------------------------------------------------
+## Do real stuff
+if command -v notify-send &> /dev/null
+then
+    notify-send -u critical --app-name="LVM Backup Script" "LVM Backup Script is running!" "Please make sure to wait until the script finished."
+fi
+
+
+#for i in {0..100..5}; do echo "$i"; sleep 0.3; done | yad --progress \
+#                                                          --progress-text "You have to admit it's getting better" \
+#                                                          --percentage=0 \
+#                                                          --auto-close
+
+
 ./backup-lvm.sh  2>&1 | tee -a "${BACKDIR}/${BACKNAM}.log"
