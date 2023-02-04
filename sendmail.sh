80df5314e226af4da44ff050e2284ff50b0b8434
Chris S <cschue88@gmail.com>
2023-02-04 09:22:44 +0000

    Added df header.

--- sendmail.sh
+++ sendmail.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MAIL="LVM Backup for Morpheus finished.\n"
 MAIL+="Backup disk space:\n"
-MAIL+=`df -h | grep "/media/backups"`
+MAIL+=`df -h /media/backups/`
 MAIL+="\n"
 
 echo -e "$MAIL" | mail -s "Morpheus Backup Results" cschue88@gmail.com
